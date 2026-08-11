662b61ca4659d96adf2bc858a15a0633302caac0
visual tail studio <christiangrice0@gmail.com>
2026-08-11 11:33:38 -0500

    started coding

---
+++ Haptic.cs
@@ -0,0 +1,15 @@
+using System;
+
+public static HapticIDE{
+
+    public static string FILE {get; set;}
+    public static string[] LINES_OF_FILE {get; set;}
+
+    public static void Main(){
+        GetFileToWrite();
+    }
+    
+    public static void GetFileToWrite(){
+        
+    }
+}
